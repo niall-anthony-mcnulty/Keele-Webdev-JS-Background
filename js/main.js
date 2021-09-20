854a5d5fbfdf534dfaa7f2a941b150e4cfec6e07
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-09-20 18:58:54 +0800

    debugged js

--- js/main.js
+++ js/main.js
@@ -1,45 +1,46 @@
-var images_portrait = ['../images/smartphone-portrait/smartphone1.jpg',
-    '../images/smartphone-portrait/smartphone2.jpg',
-    '../images/smartphone-portrait/smartphone3.jpg',
-    '../images/smartphone-portrait/smartphone4.jpg',
-    '../images/smartphone-portrait/smartphone5.jpg'];
+$(document).ready(function(){
 
-selectPortraitImages = images_portrait[Math.floor(Math.random * images_portrait.length)];
+    var images_portrait = ['./images/smartphone-portrait/smartphone1.jpg',
+        './images/smartphone-portrait/smartphone2.jpg',
+        './images/smartphone-portrait/smartphone3.jpg',
+        './images/smartphone-portrait/smartphone4.jpg',
+        './images/smartphone-portrait/smartphone5.jpg'];
 
-$('bg-image-smartphone').css({'background':'url(' + selectPortraitImages + ')'});
-    
+    selectPortraitImages = images_portrait[Math.floor(Math.random() * images_portrait.length)];
+    $('.bg-image-smartphone').css({'background-image':'url(' + selectPortraitImages + ')'}) });
+        
 
-var images_landscape = ['../images/smartphone-landscape/landscape1.jpg',
-    '../images/smartphone-landscape/landscape2.jpg',
-    '../images/smartphone-landscape/landscape3.jpg',
-    '../images/smartphone-landscape/landscape4.jpg',
-    '../images/smartphone-landscape/landscape5.jpg' ];
+$(document).ready(function(){
 
-selectLandscapeImages = images_landscape[Math.floor(Math.random * images_landscape.length)];
+    var images_landscape = ['./images/smartphone-landscape/landscape1.jpg',
+        './images/smartphone-landscape/landscape2.jpg',
+        './images/smartphone-landscape/landscape3.jpg',
+        './images/smartphone-landscape/landscape4.jpg',
+        './images/smartphone-landscape/landscape5.jpg' ];
 
-get('bg-image-landscape').css({'background':'url(' + selectLandscapeImages + ')'});
+    selectLandscapeImages = images_landscape[Math.floor(Math.random() * images_landscape.length)];
+    $('.bg-image-landscape').css({'background-image':'url(' + selectLandscapeImages + ')'}); });
 
-var images_tablet = ['../images/tablet/tablet1.jpg',
-    '../images/tablet/tablet2.jpg',
-    '../images/tablet/tablet3.jpg',
-    '../images/tablet/tablet4.jpg',
-    '../images/tablet/tablet5.jpg'];
+$(document).ready(function(){   
+    var images_tablet = ['./images/tablet/tablet1.jpg',
+        './images/tablet/tablet2.jpg',
+        './images/tablet/tablet3.jpg',
+        './images/tablet/tablet4.jpg',
+        './images/tablet/tablet5.jpg'];
 
-selectTabletImages = images_tablet[Math.floor(Math.random * images_tablet.length)];
+    selectTabletImages = images_tablet[Math.floor(Math.random() * images_tablet.length)];
+    $('.bg-image-tablet').css({'background-image':'url(' + selectTabletImages + ')'}); });
 
-$('bg-image-tablet').css({'background':'url(' + selectTabletImages + ')'}); 
+$(document).ready(function(){   
+    var images_desktop = [
+        './images/desktop/desktop1.jpg',
+        './images/desktop/desktop2.jpg',
+        './images/desktop/desktop3.jpg',
+        './images/desktop/desktop4.jpg',
+        './images/desktop/desktop5.jpg'];
 
-var images_desktop = [
-    '../images/desktop/desktop1.jpg',
-    '../images/desktop/desktop2.jpg',
-    '../images/desktop/desktop3.jpg',
-    '../images/desktop/desktop4.jpg',
-    '../images/desktop/desktop5.jpg'];
-
-selectDesktopImages = images_desktop[Math.floor(Math.random * images_desktop.length)];
-
-$('.bg-image-desktop').css({'background-image':'url(' + selectDesktopImages + ')'})
-
-
+    selectDesktopImages = images_desktop[Math.floor(Math.random() * images_desktop.length)];
+    $('.bg-image-desktop').css({'background-image':'url(' + selectDesktopImages + ')'});
 
 
+});
